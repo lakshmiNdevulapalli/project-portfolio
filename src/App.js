116b911ec0922c05a863de701080ede7125e77c6
baludevulapalli@gmail.com <baludevulapalli@gmail.com>
2021-02-14 15:04:07 -0800

    added header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,36 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css"
-import NavBar from './components/NavBar'
+import Particles from "react-particles-js";
+import NavBar from "./components/NavBar"
+import Header from "./components/Header"
+
 function App() {
   return (
-    <NavBar></NavBar>
+    <>
+    <Particles 
+      className="particles-canvas"
+      params={{
+        particles: {
+          number: {
+            value: 30,
+            density: {
+              enable: true,
+              value_area: 900
+            }
+          },
+          shape: {
+            type: "square",
+            stroke: {
+              width: 6,
+              color: "#fff"
+            }
+          }
+        }
+      }}
+    />
+    <NavBar />
+    <Header />
+    </>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import React from "react"
+import Typed from "react-typed"
+
+const Header = () => {
+    return (
+        <div className="header-wrapper">
+            <div className="main-info">
+                <Typed 
+                    className="typed-text"
+                    strings={["DevOps", "Cryptography", "BlockChain"]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">contact me</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
